240e5c35ad7e8637d74ebe12f985b3cf302c1c95
rifat.zaman <rifat.zaman@upaybd.com>
2025-02-26 16:21:00 +0600

    Remove: Obsolete OTP configuration and add new general configuration management
    - Delete outdated OTP config file
    - Introduce new configuration module using Viper for application settings
    - Update main function to load the new configuration management setup

---
+++ cmd/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBDriver   string
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+	SSLMode    string
+	TimeZone   string
+}
+
+var AppConfig *Config
+
+func LoadConfig() {
+	viper.AutomaticEnv()
+
+	AppConfig = &Config{
+		DBDriver:   viper.GetString("DB_DRIVER"),
+		DBHost:     viper.GetString("DB_HOST"),
+		DBUser:     viper.GetString("DB_USER"),
+		DBPassword: viper.GetString("DB_PASSWORD"),
+		DBName:     viper.GetString("DB_NAME"),
+		DBPort:     viper.GetString("DB_PORT"),
+		SSLMode:    viper.GetString("SSL_MODE"),
+		TimeZone:   viper.GetString("TIME_ZONE"),
+	}
+
+	log.Println("✅ Configuration loaded successfully")
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/Zaman-R/otp-validator/cmd/config"
+)
+
+func main() {
+	config.LoadConfig()
+
+}
